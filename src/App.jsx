74b7d9e838fac8811e585c6fee2ce08a9a46ea68
j-leklere <jleklere@yahoo.com.ar>
2023-11-22 18:07:24 -0300

    Commiting to pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import ReactDOM from "react-dom";
 import AboutUs from "./Components/AboutUs";
-import Footer from "./Components/Footer";
 import MobileNav from "./Components/MobileNav";
 import { useState } from "react";
-// import Header from "./Components/Header";
+import Header from "./Components/Header";
+import Intro from "./Components/Intro";
+import Services from "./Components/Services";
+import Footer from "./Components/Footer";
 import Contact from "./Components/Contact";
 
 import IntroBckg from "./Assets/intro-bckg.png";
 import ContactBckg from "./Assets/contact-bckg.png";
 import RedWave from "./Assets/wave-haikei.svg";
-import Intro from "./Components/Intro";
-import Services from "./Components/Services";
 
 const overlays = document.getElementById("overlays");
 
@@ -26,14 +26,14 @@ function App() {
       {mobileNavOpen &&
         ReactDOM.createPortal(<MobileNav onMenuClick={toggleNav} />, overlays)}
       <img src={IntroBckg} alt="intro bckg" className="pharmacy-img1" />
-      {/* <Header mobNavOpen={mobileNavOpen} onMenuClick={toggleNav} /> */}
+      <Header mobNavOpen={mobileNavOpen} onMenuClick={toggleNav} />
       <Intro />
       <AboutUs />
+      <Services />
       <Contact />
       <Footer />
       <img src={ContactBckg} alt="contact bckg" className="pharmacy-img2" />
       <img src={RedWave} alt="red background wave color" className="red-wave" />
-      <Services />
     </div>
   );
 }
